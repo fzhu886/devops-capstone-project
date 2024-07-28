6a0ec1a575e863f21848192f615bbc49feacbf65
Fan Zhu <fzhu@ualberta.ca>
2024-07-27 23:10:35 -0600

    Update routes.py, flixed style errors

--- service/routes.py
+++ service/routes.py
@@ -57,11 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
-######################################################################
-# LIST ALL ACCOUNTS
-######################################################################
 
-# ... place you code here to LIST accounts ...
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -78,6 +74,8 @@ def list_accounts():
 
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -94,8 +92,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
-
 
 
 ######################################################################
